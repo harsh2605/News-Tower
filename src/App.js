3a575a7fd9b9783741e0d4558bb4edecf8c96841
harsh <harshagarwal26052001@gmail.com>
2023-07-12 17:51:06 +0530

    made the whole App.js as functional based component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 //this whole template generates by typing rcc (for more refernce and shortcut keys you can see the page ES7 react redux by microsoft)
-import React, { Component } from 'react'
+import React from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
 import {
@@ -10,30 +10,29 @@ import {
   Route,
   Link
 } from "react-router-dom";
-export default class App extends Component {
-  PageSize = 6;
-  render() {
-    return (
+const App = () => {
+  const PageSize = 6;
 
-      <div>
-        <Router>
-          <NavBar />
-          <Switch>
-            <Route exact path="/"><News key="general" pageSize={this.PageSize} country="in" category="general" /></Route>
-            <Route exact path="/business"><News key="business" pageSize={this.PageSize} country="in" category="business" /></Route>
-            <Route exact path="/entertainment"><News key="entertainment" pageSize={this.PageSize} country="in" category="entertainment" /></Route>
-            <Route exact path="/general"><News key="general" pageSize={this.PageSize} country="in" category="general" /></Route>
-            <Route exact path="/health"><News key="health" pageSize={this.PageSize} country="in" category="health" /></Route>
-            <Route exact path="/science"><News key="science" pageSize={this.PageSize} country="in" category="science" /></Route>
-            <Route exact path="/sports"><News key="sports" pageSize={this.PageSize} country="in" category="sports" /></Route>
-            <Route exact path="/technology"><News key="technology" pageSize={this.PageSize} country="in" category="technology" /></Route>
-          </Switch>
-        </Router>
-      </div>
-    )
-  }
+  return (
+
+    <div>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path="/"><News key="general" pageSize={PageSize} country="in" category="general" /></Route>
+          <Route exact path="/business"><News key="business" pageSize={PageSize} country="in" category="business" /></Route>
+          <Route exact path="/entertainment"><News key="entertainment" pageSize={PageSize} country="in" category="entertainment" /></Route>
+          <Route exact path="/general"><News key="general" pageSize={PageSize} country="in" category="general" /></Route>
+          <Route exact path="/health"><News key="health" pageSize={PageSize} country="in" category="health" /></Route>
+          <Route exact path="/science"><News key="science" pageSize={PageSize} country="in" category="science" /></Route>
+          <Route exact path="/sports"><News key="sports" pageSize={PageSize} country="in" category="sports" /></Route>
+          <Route exact path="/technology"><News key="technology" pageSize={PageSize} country="in" category="technology" /></Route>
+        </Switch>
+      </Router>
+    </div>
+  )
 }
 
 
-
+export default App;
 
